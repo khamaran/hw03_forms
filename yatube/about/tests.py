746df1b3acb12a009699a0e1962125ba35727ff1
Мария Тумилович <khamaran@yahoo.com>
2023-01-18 19:00:17 +0300

    Test_views

--- yatube/about/tests.py
+++ yatube/about/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from http import HTTPStatus
+from django.urls import reverse
 
 
 class AboutURLTests(TestCase):
@@ -28,3 +29,30 @@ class AboutURLTests(TestCase):
             with self.subTest(address=address):
                 response = self.guest_client.get(address)
                 self.assertTemplateUsed(response, template)
+
+
+class AboutViewsTests(TestCase):
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_about_author_page_accessible_by_name(self):
+        """URL, генерируемый при помощи имени about:author, доступен."""
+        response = self.guest_client.get(reverse('about:author'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_author_page_uses_correct_template(self):
+        """При запросе к staticpages:about
+        применяется шаблон about/author.html."""
+        response = self.guest_client.get(reverse('about:author'))
+        self.assertTemplateUsed(response, 'about/author.html')
+
+    def test_about_tech_page_accessible_by_name(self):
+        """URL, генерируемый при помощи имени about:tech, доступен."""
+        response = self.guest_client.get(reverse('about:tech'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_tech_page_uses_correct_template(self):
+        """При запросе к staticpages:about
+        применяется шаблон about/tech.html."""
+        response = self.guest_client.get(reverse('about:tech'))
+        self.assertTemplateUsed(response, 'about/tech.html')
