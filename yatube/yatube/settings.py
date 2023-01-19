64795dbf739dc98e6fba2079a923b88f1688951c
Мария Тумилович <khamaran@yahoo.com>
2023-01-19 21:33:31 +0300

    Recover tests

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -135,9 +135,6 @@ POSTS_COUNT = 10
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
 
-SIGNUP_URL = 'users:signup'
-SIGNUP_REDIRECT_URL = 'posts:index'
-
 #  подключаем движок filebased.EmailBackend
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 # указываем директорию, в которую будут складываться файлы писем
